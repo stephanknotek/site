c3b4a5d96fb0966a7715428a24c0dd3a14766f7c
stephanknotek <knoteks6360@my.uwstout.edu>
2022-03-31 17:50:47 -0500

    PHP 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <title>PHP</title>
+  </head>
+
+<body>
+<h2>PHP</h2>
+
+<?php
+	$input = $_POST["userInput"];
+	
+	echo "<p>You printed: </p>
+	<h1>$input</h1>
+		";
+?>
+	</tr>
+</table>
+</body>
+</html>
